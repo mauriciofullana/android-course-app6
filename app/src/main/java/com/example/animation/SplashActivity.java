b7c156d9b94415bc6a5f65a18e807d56d1bc73a0
Mauricio Fullana <mfullana4@gmail.com>
2020-12-19 19:01:00 -0300

    Animated Splash Activity

---
+++ app/src/main/java/com/example/animation/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        ImageView icon = findViewById(R.id.icon);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.rotation);
+        icon.startAnimation(animation);
+    }
+}
